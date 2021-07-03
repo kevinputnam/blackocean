a0a6afb36656c3d248131711128168c4edc33626
Kevin Putnam <kevin.putnam@gmail.com>
2021-07-02 23:27:13 -0700

    Possible fix for weird problem.
    Signed-off-by: Kevin Putnam <kevin.putnam@gmail.com>

--- reDocs.py
+++ reDocs.py
@@ -200,9 +200,10 @@ class reDocs:
         for item in listdir(self.sourcePath):
             itemPath = join(self.sourcePath,item)
             if isfile(itemPath) and item.endswith(".rst"):
-                htmlTitle = ""
                 if item not in ignoreFiles:
                     htmlTitle, docSections, htmlFileName = self.convertDoc(item)
+                else:
+                    next
                 fileList.append(item)
                 if htmlTitle == "" or htmlTitle in newFileDict:
                     htmlTitle = htmlTitle + " " + str(noTitleCounter)
